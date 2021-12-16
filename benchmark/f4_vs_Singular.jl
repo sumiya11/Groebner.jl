862cc157408870054ee082e6760535fe281dbee9
sumiya11 <alexd382382@mail.ru>
2021-12-16 04:41:27 +0300

    update benchmarks

--- benchmark/f4_vs_Singular.jl
+++ benchmark/f4_vs_Singular.jl
@@ -1,20 +1,14 @@
 
 include("../src/GroebnerBases.jl")
 using .GroebnerBases
+using GroebnerBases: katsura6, change_ordering, rootn
 
-import AbstractAlgebra
+# hehe
 import Singular
+import AbstractAlgebra
 
 using BenchmarkTools
 
-
-function run_ours(system)
-    @benchmark gb = GroebnerBases.f4(system)
-end
-
-function run_singular(system)
-    @benchmark gb = Singular.std(system)
-end
 
 function benchmark_katsura()
     modulo = 2^31 - 1
@@ -24,11 +18,50 @@ function benchmark_katsura()
     R_s, _ = Singular.PolynomialRing(ground_s, ["x$i" for i in 1:7], ordering=:degrevlex)
 
     system_s = map(
-        f -> change_base_ring(ground_s, f, parent=R_s),
+        f -> AbstractAlgebra.change_base_ring(
+                    ground_s,
+                    AbstractAlgebra.map_coefficients(c -> ground_s(c.d), f),
+                    parent=R_s),
         system)
 
     ideal_s = Singular.Ideal(R_s, system_s)
 
-    run_ours(system)
-    run_singular(ideal_s)
+    GroebnerBases.f4([system[1]])
+    Singular.std(Singular.Ideal(R_s, [system_s[1]]))
+
+    @benchmark gb1 = GroebnerBases.f4($system)
+    @benchmark gb2 = Singular.std($ideal_s)
+
+    println(gb1)
+    println("###############")
+    println(gb2)
 end
+
+function benchmark_root()
+    modulo = 2^31 - 1
+    system = change_ordering(rootn(5, ground=AbstractAlgebra.GF(modulo), :degrevlex))
+
+    ground_s = Singular.N_ZpField(modulo)
+    R_s, _ = Singular.PolynomialRing(ground_s, ["x$i" for i in 1:5], ordering=:degrevlex)
+
+    system_s = map(
+        f -> AbstractAlgebra.change_base_ring(
+                    ground_s,
+                    AbstractAlgebra.map_coefficients(c -> ground_s(c.d), f),
+                    parent=R_s),
+        system)
+
+    ideal_s = Singular.Ideal(R_s, system_s)
+
+    GroebnerBases.f4([system[1]])
+    Singular.std(Singular.Ideal(R_s, [system_s[1]]))
+
+    @benchmark gb1 = GroebnerBases.f4($system)
+    @benchmark gb2 = Singular.std($ideal_s)
+
+    println(gb1)
+    println("###############")
+    println(gb2)
+end
+
+benchmark_root()
