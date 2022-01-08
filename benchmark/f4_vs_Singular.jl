e1caebaa838d82487413f55d71f4a0c2cb5ce288
sumiya11 <alexd382382@mail.ru>
2022-01-08 08:34:57 +0300

    update benchmarks x2

--- benchmark/f4_vs_Singular.jl
+++ benchmark/f4_vs_Singular.jl
@@ -1,18 +1,24 @@
 
-
-# hehe
 import Singular
 import AbstractAlgebra
 
 using BenchmarkTools
 
+using Logging
+global_logger(ConsoleLogger(stderr, Logging.Error))
 
-function benchmark_katsura()
-    modulo = 2^31 - 1
-    system = change_ordering(katsura6(ground=AbstractAlgebra.GF(modulo)), :degrevlex)
+function benchmark_system_my(system)
+    system = GroebnerBases.change_ordering(system, :degrevlex)
+    GroebnerBases.groebner([system[1]])
+    @btime gb = GroebnerBases.groebner($system)
+end
 
+function benchmark_system_singular(system)
+    R = parent(system[1])
+    modulo = characteristic(R)
+    n = nvars(R)
     ground_s = Singular.N_ZpField(modulo)
-    R_s, _ = Singular.PolynomialRing(ground_s, ["x$i" for i in 1:7], ordering=:degrevlex)
+    R_s, _ = Singular.PolynomialRing(ground_s, ["x$i" for i in 1:n], ordering=:degrevlex)
 
     system_s = map(
         f -> AbstractAlgebra.change_base_ring(
@@ -23,37 +29,27 @@ function benchmark_katsura()
 
     ideal_s = Singular.Ideal(R_s, system_s)
 
-    GroebnerBases.f4([system[1]])
     Singular.std(Singular.Ideal(R_s, [system_s[1]]))
 
-    @benchmark gb1 = GroebnerBases.f4($system)
-    @benchmark gb2 = Singular.std($ideal_s)
-
+    @btime gb = Singular.std($ideal_s)
 end
 
-function benchmark_root()
-    modulo = 2^31 - 1
-    system = change_ordering(rootn(13, ground=AbstractAlgebra.GF(modulo)), :degrevlex)
+function run_f4_ff_degrevlex_benchmarks()
+    ground = AbstractAlgebra.GF(2^31 - 1)
+    systems = [
+        ("cyclic 12", GroebnerBases.rootn(12, ground=ground)),
+        ("cyclic 13", GroebnerBases.rootn(13, ground=ground)),
+        ("katsura 9", GroebnerBases.katsura9(ground=ground)),
+        ("katsura 10",GroebnerBases.katsura10(ground=ground)),
+        ("noon 6"    ,GroebnerBases.noonn(6, ground=ground)),
+        ("noon 7"    ,GroebnerBases.noonn(7, ground=ground))
+    ]
 
-    ground_s = Singular.N_ZpField(modulo)
-    R_s, _ = Singular.PolynomialRing(ground_s, ["x$i" for i in 1:13], ordering=:degrevlex)
-
-    system_s = map(
-        f -> AbstractAlgebra.change_base_ring(
-                    ground_s,
-                    AbstractAlgebra.map_coefficients(c -> ground_s(c.d), f),
-                    parent=R_s),
-        system)
-
-    ideal_s = Singular.Ideal(R_s, system_s)
-
-    GroebnerBases.f4([system[1]])
-    Singular.std(Singular.Ideal(R_s, [system_s[1]]))
-
-    @benchmark gb1 = GroebnerBases.f4($system)
-    @benchmark gb2 = Singular.std($ideal_s)
-
+    for (name, system) in systems
+        println("$name")
+        println("my")
+        benchmark_system_my(system)
+        println("singular")
+        benchmark_system_singular(system)
+    end
 end
-
-benchmark_katsura()
-benchmark_root()
--- src/testgens.jl
+++ src/testgens.jl
@@ -42,6 +42,39 @@ function katsura6(;ground=QQ)
     fs
 end
 
+function katsura9(;ground=QQ)
+    R, (x1, x2, x3, x4, x5, x6, x7, x8, x9) = PolynomialRing(ground, ["x$i" for i in 1:9])
+
+    fs = [
+        x1+2*x2+2*x3+2*x4+2*x5+2*x6+2*x7+2*x8+2*x9-1,
+        x1^2+2*x2^2+2*x3^2+2*x4^2+2*x5^2+2*x6^2+2*x7^2+2*x8^2+2*x9^2-x1,
+        2*x1*x2+2*x2*x3+2*x3*x4+2*x4*x5+2*x5*x6+2*x6*x7+2*x7*x8+2*x8*x9-x2,
+        x2^2+2*x1*x3+2*x2*x4+2*x3*x5+2*x4*x6+2*x5*x7+2*x6*x8+2*x7*x9-x3,
+        2*x2*x3+2*x1*x4+2*x2*x5+2*x3*x6+2*x4*x7+2*x5*x8+2*x6*x9-x4,
+        x3^2+2*x2*x4+2*x1*x5+2*x2*x6+2*x3*x7+2*x4*x8+2*x5*x9-x5,
+        2*x3*x4+2*x2*x5+2*x1*x6+2*x2*x7+2*x3*x8+2*x4*x9-x6,
+        x4^2+2*x3*x5+2*x2*x6+2*x1*x7+2*x2*x8+2*x3*x9-x7,
+        2*x4*x5+2*x3*x6+2*x2*x7+2*x1*x8+2*x2*x9-x8
+    ]
+end
+
+function katsura10(;ground=QQ)
+    R, (x1, x2, x3, x4, x5, x6, x7, x8, x9, x10) = PolynomialRing(ground, ["x$i" for i in 1:10])
+
+    fs = [
+        x1+2*x2+2*x3+2*x4+2*x5+2*x6+2*x7+2*x8+2*x9+2*x10-1,
+        x1^2+2*x2^2+2*x3^2+2*x4^2+2*x5^2+2*x6^2+2*x7^2+2*x8^2+2*x9^2+2*x10^2-x1,
+        2*x1*x2+2*x2*x3+2*x3*x4+2*x4*x5+2*x5*x6+2*x6*x7+2*x7*x8+2*x8*x9+2*x9*x10-x2,
+        x2^2+2*x1*x3+2*x2*x4+2*x3*x5+2*x4*x6+2*x5*x7+2*x6*x8+2*x7*x9+2*x8*x10-x3,
+        2*x2*x3+2*x1*x4+2*x2*x5+2*x3*x6+2*x4*x7+2*x5*x8+2*x6*x9+2*x7*x10-x4,
+        x3^2+2*x2*x4+2*x1*x5+2*x2*x6+2*x3*x7+2*x4*x8+2*x5*x9+2*x6*x10-x5,
+        2*x3*x4+2*x2*x5+2*x1*x6+2*x2*x7+2*x3*x8+2*x4*x9+2*x5*x10-x6,
+        x4^2+2*x3*x5+2*x2*x6+2*x1*x7+2*x2*x8+2*x3*x9+2*x4*x10-x7,
+        2*x4*x5+2*x3*x6+2*x2*x7+2*x1*x8+2*x2*x9+2*x3*x10-x8,
+        x5^2+2*x4*x6+2*x3*x7+2*x2*x8+2*x1*x9+2*x2*x10-x9
+    ]
+end
+
 function eco5()
     R, (x1, x2, x3, x4, x5) = PolynomialRing(GF(1073741827), ["x$i" for i in 1:5])
 
