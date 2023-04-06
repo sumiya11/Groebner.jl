e34c1c27fa7363d663b3a25218bb9173d9cbeba0
sumiya11 <alexd382382@mail.ru>
2023-04-06 07:30:13 +0300

    add more benchmark systems to CI

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,14 +6,23 @@ using Groebner, AbstractAlgebra
 
 const SUITE = BenchmarkGroup()
 
-systems = Dict(
-    "katsura-8" => Groebner.katsuran(8, ground=GF(2^31-1), ordering=:degrevlex),
-    "cyclic-7" => Groebner.cyclicn(7, ground=GF(2^31-1), ordering=:degrevlex)
+K = GF(2^31-1)
+systems_over_ff = Dict(
+    "katsura-8" => Groebner.katsuran(8, ground=K, ordering=:degrevlex),
+    "katsura-9" => Groebner.katsuran(9, ground=K, ordering=:degrevlex),
+    "cyclic-7" => Groebner.cyclicn(7, ground=K, ordering=:degrevlex),
+    "cyclic-8" => Groebner.cyclicn(8, ground=K, ordering=:degrevlex),
+    "noon-7"  => Groebner.noonn(7, ground=K, ordering=:degrevlex),
+    "noon-8"  => Groebner.noonn(8, ground=K, ordering=:degrevlex),
+    "eco-10"  => Groebner.eco10(ground=K, ordering=:degrevlex),
+    "eco-11"  => Groebner.eco11(ground=K, ordering=:degrevlex)
 )
+
+run_system(system; kwargs...) = Groebner.groebner(system, linalg=:prob, kwargs...)
 
 SUITE["groebner"] = BenchmarkGroup()
 SUITE["groebner"]["finite-field"] = BenchmarkGroup()
 
-SUITE["groebner"]["finite-field"]["katsura-8"] = @benchmarkable Groebner.groebner($systems["katsura-8"], linalg=:prob, loglevel=Logging.Warn)
-SUITE["groebner"]["finite-field"]["cyclic-7"] = @benchmarkable Groebner.groebner($systems["cyclic-7"], linalg=:prob, loglevel=Logging.Warn)
-
+for (name, system) in systems_over_ff
+    SUITE["groebner"]["finite-field"][name] = @benchmarkable run_system($system)
+end
