df4828e764b036e99d191f00ff13997353a93dbc
Alexander Demin <asdemin_2@edu.hse.ru>
2023-07-11 23:31:05 +0300

    using CI for runtests

--- src/utils/logging.jl
+++ src/utils/logging.jl
@@ -8,7 +8,7 @@
 const _default_logger = @static if VERSION >= v"1.8.0"
     Ref{Logging.ConsoleLogger}(Logging.ConsoleLogger(Logging.Info, show_limited=false))
 else
-    Ref{Logging.ConsoleLogger}(Logging.ConsoleLogger(Logging.Info))
+    Ref{Logging.ConsoleLogger}(Logging.ConsoleLogger())
 end
 const _default_message_loglevel = LogLevel(0)
 
