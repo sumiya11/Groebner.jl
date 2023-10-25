0a4a5d0905a83d70edff612ade4ce060ea06c4cb
Alexander Demin <asdemin_2@edu.hse.ru>
2023-10-26 00:22:01 +0300

    fix CI script

---
+++ test/performance/runtests.jl
@@ -0,0 +1,11 @@
+using Test
+using TestSetExtensions
+
+using AbstractAlgebra
+using Random
+using Groebner
+
+@testset "All performance tests" verbose = true begin
+    @info "Start benchmarking"
+    @test 1 == 1
+end
