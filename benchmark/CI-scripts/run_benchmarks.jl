c84e6352d4202e9fdb338f051cd3bc3471dfd851
Sasha Demin <demin@lix.polytechnique.fr>
2024-08-31 18:21:09 +0200

    run benchmarks

--- benchmark/CI-scripts/run_benchmarks.jl
+++ benchmark/CI-scripts/run_benchmarks.jl
@@ -64,7 +64,7 @@ function nemo_make_prime_finite_field(p)
 end
 
 function compute_gb(system, trials=7; kws...)
-    kws = Dict(kws)
+    kws = Dict{Symbol,Any}(kws)
     !haskey(kws, :threaded) && (kws[:threaded] = :no) 
     times = []
     for _ in 1:trials
