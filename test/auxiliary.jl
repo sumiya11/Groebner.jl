6a5616c836daed66e5b718c3a015ddf959cd9fcf
Sasha Demin <asdemin_2@edu.hse.ru>
2025-01-05 14:52:30 +0100

    correct tests for lead

--- test/auxiliary.jl
+++ test/auxiliary.jl
@@ -9,8 +9,8 @@ using Test
     @test Groebner.lead(2x - 3y, ordering=Lex(y, x)) == -3y
 
     R, (X, Y) = fraction_field(R)["X", "Y"]
-    @test Groebner.lead(2 * x * Y - 3 * y * X, ordering=Lex(X, Y)) == -3 * y
-    @test Groebner.lead(2 * x * Y - 3 * y * X, ordering=Lex(Y, X)) == 2 * x
+    @test Groebner.lead(2 * x * Y - 3 * y * X, ordering=Lex(X, Y)) == -3 * y * X
+    @test Groebner.lead(2 * x * Y - 3 * y * X, ordering=Lex(Y, X)) == 2 * x * Y
     @test Groebner.lead(zero(R)) == zero(R)
     @test Groebner.lead(-one(R)) == -one(R)
 end
