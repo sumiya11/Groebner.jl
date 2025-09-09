1b57606056062de8c331cf21950a2c108617a244
Alexander Demin <demin@r04n02.mpi-cbg.de>
2025-09-09 07:27:16 +0200

    add discourse post

--- scripts/crt/benchmark_crt.jl
+++ scripts/crt/benchmark_crt.jl
@@ -1,20 +1,41 @@
-using Revise
-using Primes, Groebner
+using Base.Threads
 
-if false
-M = 1024
-P = 101
-modulo = BigInt()
-table_zz = [[BigInt() for _ in 1:i] for i in 1:M]
-moduli = UInt64.(prevprimes(2^31-1, P))
-tables_ff = [ [[mod(rand(UInt64), moduli[j]) for _ in 1:i] for i in 1:M] for j in 1:P]
-mask = [falses(i) for i in 1:M]
+function crt!(M, buf, n1, n2, ai, ci)
+    Base.GMP.MPZ.set_ui!(n1, UInt(0))
+    for i in 1:length(ai)
+        Base.GMP.MPZ.mul_ui!(n2, ci[i], ai[i])
+        Base.GMP.MPZ.add!(n1, n2)
+    end
+    Base.GMP.MPZ.set!(buf, n1)
+    Base.GMP.MPZ.fdiv_r!(buf, M)
 end
 
-@time Groebner.crt_vec_full!(
-    table_zz,
-    modulo,
-    tables_ff,
-    moduli,
-    mask
-)
+function crt_vec_full!(table_zz, modulo, tables_ff, mults, moduli)
+    NN = Threads.maxthreadid()
+    buf = [BigInt() for _ in 1:NN]
+    n1 = [BigInt() for _ in 1:NN]
+    n2 = [BigInt() for _ in 1:NN]
+    rems = [Vector{UInt64}(undef, length(moduli)) for _ in 1:NN]
+    Threads.@threads :static for i in 1:length(table_zz)
+        t = Threads.threadid()
+        for j in 1:length(table_zz[i])
+            for k in 1:length(moduli)
+                rems[t][k] = UInt64(tables_ff[k][i][j])
+            end
+            crt!(modulo, buf[t], n1[t], n2[t], rems[t], mults)
+            Base.GMP.MPZ.set!(table_zz[i][j], buf[t])
+        end
+    end
+    @assert NN == Threads.maxthreadid()
+end
+
+# Preallocate data
+M, P = 512, 101
+table_zz = [[BigInt() for _ in 1:M] for i in 1:M]
+moduli = UInt64.(prevprimes(2^31-1, P))
+modulo = prod(BigInt, moduli)
+mults = [rand(1:modulo-1) for _ in 1:length(moduli)]
+tables_ff = [ [[mod(rand(UInt64), moduli[j]) for _ in 1:M] for i in 1:M] for j in 1:P]
+
+@time crt_vec_full!(table_zz,modulo,tables_ff,mults,moduli)
+@time crt_vec_full!(table_zz,modulo,tables_ff,mults,moduli)
--- scripts/crt/new_crt.jl
+++ scripts/crt/new_crt.jl
@@ -1,64 +1,28 @@
-# Table of big integers must be initialized.
-@timeit _TIMER function crt_vec_full!(
-    table_zz::Vector{Vector{BigInt}},
-    modulo::BigInt,
-    tables_ff::Vector{Vector{Vector{T}}},
-    moduli::Vector{U},
-    mask::Vector{BitVector}
-) where {T <: Integer, U <: Integer}
-    @invariant length(tables_ff) == length(moduli)
-    @invariant length(table_zz) == length(mask)
+function crt!(M, buf, n1, n2, ai, ci)
+    Base.GMP.MPZ.set_ui!(n1, UInt(0))
+    for i in 1:length(ai)
+        Base.GMP.MPZ.mul_ui!(n2, ci[i], ai[i])
+        Base.GMP.MPZ.add!(n1, n2)
+    end
+    Base.GMP.MPZ.set!(buf, n1)
+    Base.GMP.MPZ.fdiv_r!(buf, M)
+end
 
-    NN = Threads.threadpoolsize() + 1
+function crt_vec_full!(table_zz, modulo, tables_ff, mults, moduli)
+    NN = Threads.maxthreadid()
     buf = [BigInt() for _ in 1:NN]
     n1 = [BigInt() for _ in 1:NN]
     n2 = [BigInt() for _ in 1:NN]
-
-    mults = [Vector{BigInt}(undef, length(moduli)) for _ in 1:NN]
-    for i in 1:length(mults[1])
-        for t in 1:NN
-            mults[t][i] = BigInt(0)
-        end
-    end
-    # buf, n1, n2 = BigInt(), BigInt(), BigInt()
-    # mults = Vector{BigInt}(undef, length(moduli))
-    # for i in 1:length(mults)
-    #     mults[i] = BigInt(0)
-    # end
-
-    crt_precompute!(modulo, n1[1], n2[1], mults[1], map(UInt64, moduli))
-    for t in 2:NN
-        Base.GMP.MPZ.set!(n1[t], n1[1])
-        Base.GMP.MPZ.set!(n2[t], n2[1])
-        for i in 1:length(mults[1])
-            Base.GMP.MPZ.set!(mults[t][i], mults[1][i])
-        end
-    end
-
-    # for t in 1:NN
-    #     crt_precompute!(modulo, n1[t], n2[t], mults[t], map(UInt64, moduli))
-    # end
-    # crt_precompute!(modulo, n1, n2, mults, map(UInt64, moduli))
-
-    @info "[crt] number of threads: $(NN)"
-
-    # ind = Random.shuffle(1:length(table_zz)) # collect(1:length(table_zz)) 
     rems = [Vector{UInt64}(undef, length(moduli)) for _ in 1:NN]
-    # rems = Vector{UInt64}(undef, length(moduli))
-    @time for i in 1:length(table_zz)
+    Threads.@threads :static for i in 1:length(table_zz)
         t = Threads.threadid()
-        # Core.print("[thread $t] processing row $i / $(length(table_zz))\n")
         for j in 1:length(table_zz[i])
-            mask[i][j] && continue
-
             for k in 1:length(moduli)
-                @invariant 0 <= tables_ff[k][i][j] < moduli[k]
                 rems[t][k] = UInt64(tables_ff[k][i][j])
             end
-
-            crt!(modulo, buf[t], n1[t], n2[t], rems[t], mults[t])
-
+            crt!(modulo, buf[t], n1[t], n2[t], rems[t], mults)
             Base.GMP.MPZ.set!(table_zz[i][j], buf[t])
         end
     end
+    @assert NN == Threads.maxthreadid()
 end
