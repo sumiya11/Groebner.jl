0527b6caf26ba2f980971d07592bd4c56a1146a2
Alexander Demin <demin@r04n02.mpi-cbg.de>
2025-09-12 09:51:39 +0200

    add ratrec test

--- test/ratrec.jl
+++ test/ratrec.jl
@@ -1,6 +1,19 @@
 using Test, Groebner
 
-@testset "CRT table" begin
+@testset "ratrec" begin
+    ZZ = Groebner.Nemo.ZZRingElem
+    m = ZZ(2^30 + 3)
+    p, q = -8, 111
+    a = ZZ(mod(p*invmod(ZZ(q), m), m))
+    N = ZZ((isqrt(m) >> 1) - 1)
+    D = ZZ((isqrt(m) >> 1) - 1)
+    @test Groebner.ratrec_nemo(a, m, N, D) == (true, Rational{BigInt}(p//q))
+    p, q = N + 2, D + 1
+    a = ZZ(mod(p*invmod(ZZ(q), m), m))
+    @test Groebner.ratrec_nemo(a, m, N, D)[1] == false
+end
+
+@testset "ratrec table" begin
     table_qq = [Rational{BigInt}[], Rational{BigInt}[0,0,0], Rational{BigInt}[0,0]]
     res = [Rational{BigInt}[], Rational{BigInt}[2//3, 0//1, -5//8], Rational{BigInt}[-11//1, 7//4]]
     
